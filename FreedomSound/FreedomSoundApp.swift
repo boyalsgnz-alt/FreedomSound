836239e742ac98e109ffbb38ca558b44995cdda4
Gaëtan Boyals <boyalsgnz@gmail.com>
2026-04-05 18:56:49 +1200

    Initial Commit

---
+++ FreedomSound/FreedomSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreedomSoundApp.swift
+//  FreedomSound
+//
+//  Created by Gaëtan Boyals on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FreedomSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FreedomSoundTests/FreedomSoundTests.swift
@@ -0,0 +1,19 @@
+//
+//  FreedomSoundTests.swift
+//  FreedomSoundTests
+//
+//  Created by Gaëtan Boyals on 05/04/2026.
+//
+
+import Testing
+@testable import FreedomSound
+
+struct FreedomSoundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
