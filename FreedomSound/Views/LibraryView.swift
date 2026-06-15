0a3b752a3ce186898c477342f4a1116d6efa72da
Gaëtan Boyals <boyalsgnz@gmail.com>
2026-06-15 15:23:04 +0800

    removing unecessary code + update gitignore

--- FreedomSound/Views/LibraryView.swift
+++ FreedomSound/Views/LibraryView.swift
@@ -7,121 +7,15 @@
 
 import SwiftUI
 
-struct RowHighlightButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .contentShape(Rectangle())
-        // Changes color dynamically based on configuration.isPressed
-            .background(configuration.isPressed ? Color.gray.opacity(0.2) : Color.clear)
-    }
-}
-
 struct LibraryView: View {
     @State private var showingFolderPicker = false
     @State private var path = NavigationPath()
     @EnvironmentObject var manager: FolderAccessManager
     @EnvironmentObject var router: Router
-    @State private var search = "Search"
     
     @State private var items = ["All Songs", "Playlists"]
     
     var body: some View {
-        //        VStack(spacing: 0) {
-        //
-        //            NavigationStack(path: $path) {
-        //                List {
-        //                    Button {
-        //                        path.append(AppScreen.viewtester)
-        //                    }
-        //                    label: {
-        //                        Text("All Songs")
-        //                    }
-        //                    .navigationDestination(for: AppScreen.self) { _ in
-        //                        SongListView(
-        //                            title: "All Songs",
-        //                            songs: manager.musicFiles,
-        //                            onBack: { router.goToHome() }
-        //                        ) // Destination
-        //                    }
-        //                    .navigationTitle("Library")
-        //                }
-        //                .listStyle(.plain)
-        //                .sheet(isPresented: $showingFolderPicker) {
-        //                    FolderPicker { url in
-        //                        manager.savePickedFolder(url)
-        //                        manager.scanFolder()
-        //                        showingFolderPicker = false
-        //                    }
-        //                }
-        //                  Button {
-        //                      path.append(AppScreen.viewtester)
-        //                  } label: {
-        //                    Text("ViewTester") // Source
-        //                  }
-        //                  .navigationDestination(for: AppScreen.self) { _ in
-        //                      SongListView(
-        //                          title: "All Songs",
-        //                          songs: manager.musicFiles,
-        //                          onBack: { router.goToHome() }
-        //                      ) // Destination
-        //                  }
-        //                }
-        //            Text("Your library")
-        //                .font(.title)
-        //                .frame(maxWidth: .infinity, alignment: .center)
-        //                .padding()
-        //
-        //            List {
-        //                Button {
-        //                    router.goToViewTester()
-        //                }
-        //                label: {
-        //                    HStack(spacing: 12) {
-        //                        Text("All Songs")
-        //                        Spacer()
-        //                        Image(systemName: "chevron.right")
-        //                            .font(.caption)
-        //                            .foregroundStyle(.secondary)
-        //                    }
-        //                }
-        //                .contentShape(Rectangle())
-        //
-        //                RowButtonTest(minHeight: 34) {
-        //                    router.goToViewTester()
-        //                } content: {
-        //                    HStack(spacing: 12) {
-        //                        Text("All Songs")
-        //                        Spacer()
-        //                        Image(systemName: "chevron.right")
-        //                            .font(.caption)
-        //                            .foregroundStyle(.secondary)
-        //                    }
-        //                }
-        //                .contentShape(Rectangle())
-        //
-        //                RowButtonTest(minHeight: 34) {
-        //                    router.goToPlaylists()
-        //                } content: {
-        //                    HStack(spacing: 12) {
-        //                        Text("Playlists")
-        //                        Spacer()
-        //                        Image(systemName: "chevron.right")
-        //                            .font(.caption)
-        //                            .foregroundStyle(.secondary)
-        //                    }
-        //                }
-        //                .contentShape(Rectangle())
-        //            }
-        //            .listStyle(.plain)
-        //            .sheet(isPresented: $showingFolderPicker) {
-        //                FolderPicker { url in
-        //                    manager.savePickedFolder(url)
-        //                    manager.scanFolder()
-        //                    showingFolderPicker = false
-        //                }
-        //            }
-        //        }
         NavigationStack(path: $path) {
             VStack(spacing: 0) {
                 
@@ -143,7 +37,6 @@ struct LibraryView: View {
                 }
                 .listStyle(.plain)
             }
-            .navigationTitle("Library")
         }
     }
 }
